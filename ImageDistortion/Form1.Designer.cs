04ddf068a1aef6e859182d2fbb925c857b3a376c
Ha Duc <haducvcvb@gmail.com>
2022-11-07 13:03:11 +0700

    Xây dựng UI #1 - Open Image, Remove Image, Save Image

--- ImageDistortion/Form1.Designer.cs
+++ ImageDistortion/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ImageDistortion
 {
-    partial class Form1
+    partial class ImageDistortion
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.delBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(12, 12);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(124, 43);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "Open Image";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(307, 12);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(124, 43);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save Image";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 373);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // delBtn
+            // 
+            this.delBtn.Location = new System.Drawing.Point(157, 12);
+            this.delBtn.Name = "delBtn";
+            this.delBtn.Size = new System.Drawing.Size(124, 43);
+            this.delBtn.TabIndex = 2;
+            this.delBtn.Text = "Delete Image";
+            this.delBtn.UseVisualStyleBackColor = true;
+            this.delBtn.Click += new System.EventHandler(this.delBtn_Click);
+            // 
+            // ImageDistortion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.delBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.openBtn);
+            this.Name = "ImageDistortion";
+            this.Text = "ImageDistortion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button openBtn;
+        private Button saveBtn;
+        private PictureBox pictureBox1;
+        private Button delBtn;
     }
 }
--- ImageDistortion/Form1.cs
+++ ImageDistortion/Form1.cs
@@ -1,10 +1,55 @@
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
 namespace ImageDistortion
 {
-    public partial class Form1 : Form
+    public partial class ImageDistortion : Form
     {
-        public Form1()
+        Image image;
+        Bitmap bitmap;
+
+        public ImageDistortion()
         {
             InitializeComponent();
+
+            //Bitmap bitmap = new Bitmap(@"D:\Coding\CSharp\XuLyAnh\ImageDistortion\img\mai_lan.jpg");
+
+            //pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            //pictureBox1.Image = bitmap;
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+            DialogResult dlg = ofd.ShowDialog();
+
+            if (dlg == DialogResult.OK)
+            {
+                image = Image.FromFile(ofd.FileName);
+                bitmap = (Bitmap)image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = bitmap;
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = @"PNG|*.png}";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName, ImageFormat.Png);
+            }
+        }
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
         }
     }
 }
